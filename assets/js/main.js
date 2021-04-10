427ae4034e0eba83b5436d6f76b5a4f257e3f8f1
JoeTuohy <joe.tuohy89@gmail.com>
2021-04-10 17:49:26 +0000

    added help section and JS/CSS to show/hide it

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,10 +1,18 @@
 function toggleMenu() {
     var status = $("#menu-list").css("display");
-    console.log(status);
     if(status == "none") {
         var newStatus = "block";
     } else {
         var newStatus = "none";
     }
     $("#menu-list").css("display", newStatus);
+}
+
+function showHelp() {
+    $("#menu-list").css("display", "none");
+    $("#help-section").css("display", "block");
+}
+
+function hideHelp() {
+    $("#help-section").css("display", "none");
 }
