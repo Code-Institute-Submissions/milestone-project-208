aa3a40b103140e34dc97ed922dc5ee786f16d9dc
JoeTuohy <joe.tuohy89@gmail.com>
2021-04-11 12:59:40 +0000

    uploaded images, reindexed rows/columns in HTML to start at 0, added js logic to select x number of random cards required

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,6 +1,6 @@
 function toggleMenu() {
-    var status = $("#menu-list").css("display");
-    var newStatus = status == "none" ? "block" : "none";
+    let status = $("#menu-list").css("display");
+    let newStatus = status == "none" ? "block" : "none";
     $("#menu-list").css("display", newStatus);
 }
 
@@ -13,9 +13,33 @@ function hideHelp() {
     $("#help-section").css("display", "none");
 }
 
-function setup() {
+const difficulties = {"easy":4,"medium":5,"hard":6};
+
+function setup(difficulty) {
+    let columns = difficulties[difficulty];
+    buildCardDeck(columns);
+
     $(".card-cell").html('<img class="card-image" src="assets/images/cardBack_red2.png" alt="back of card">')
 }
+
+function buildCardDeck(columns) {
+    let suits = ["Clubs","Diamonds","Hearts","Spades"];
+    let values = ["A","2","3","4","5","6","7","8","9","10","J","Q","K"];
+    let deck = [];
+    suits.forEach(function(suit) {
+        values.forEach(function(value) {
+            deck.push(`card${suit}${value}`);
+        });
+    });
+    let limit = 51;
+    let cardsChosen = [];
+    for(var x = 0; x < (columns * 4); x++) {
+        var rand = Math.floor(Math.random() * limit);
+        limit--;
+        cardsChosen.push(deck.splice(rand,1)[0])
+    }
+}
+
 $(document).ready(function() {
-    setup();
+    setup("medium");
 });
